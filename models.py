e45f0ed035032e5e4db0dd88f98f716a20f74045
Benjamin Golder <benjamin.j.golder@gmail.com>
2012-04-24 18:56:32 +0200

    basic models

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return '%s: %s' % (self.code, self.name)
+
+class TextItem(models.model):
+    name = models.CharField(max_length=200)
+    language = models.ForeignKey(Language)
+    content = models.TextField()
+    is_markdown = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.name, self.language.code)
+
+
